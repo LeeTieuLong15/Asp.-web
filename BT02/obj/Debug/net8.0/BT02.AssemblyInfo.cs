e86bb6e2df25c5c7e6b7f58b8072ae315670f0db
LyTieuLong15 <1822020129@dntu.edu.vn>
2024-08-27 15:09:27 +0700

    Them file BaiTap02 va BaiTap03

--- BT02/obj/Debug/net8.0/BT02.AssemblyInfo.cs
+++ BT02/obj/Debug/net8.0/BT02.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BT02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+896c268e20c87d84212667107efaadebb4fa4b48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84fbbaffe086cc33d451b3af24fea17d9e7e6ddb")]
 [assembly: System.Reflection.AssemblyProductAttribute("BT02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BT02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
