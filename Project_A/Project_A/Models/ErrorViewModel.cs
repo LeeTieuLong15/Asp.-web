313cffaee57165faab3d152aa997a5889ad43eab
Lê Phước Hậu <144966057+LeeTieuLong15@users.noreply.github.com>
2024-11-04 08:39:37 +0700

    Delete Project_A directory

--- Project_A/Project_A/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Project_A.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
